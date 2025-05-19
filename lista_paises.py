c7a573c0fbf98b5dce0f0abcd18a7bc62d8c2428
jordillobre <50888832+jordillobre@users.noreply.github.com>
2025-05-19 21:34:46 +0200

    incorporación de países 01
    Incorporación de más países al listado existente y cambio de color al sistema hexadecimal

--- lista_paises.py
+++ lista_paises.py
@@ -1,51 +1,93 @@
 paises = [
     {
         "nombre": "España",
-        "ids": ["españa", "es", "esp"],
+        "ids": ["españa", "es", "esp", "spain"],
         "tipo_bandera": "tipo_1",
-        "colores": ["red", "yellow", "red"],
-        "fondo": "white"
+        "colores": ["#AA151B", "#F1BF00", "#AA151B"],
+        "fondo": "#FFFFFF"
     },
     {
         "nombre": "Alemania",
-        "ids": ["alemania", "de", "ger"],
+        "ids": ["alemania", "de", "ger", "germany"],
         "tipo_bandera": "tipo_1",
-        "colores": ["black", "red", "yellow"],
-        "fondo": "white"
+        "colores": ["#000000", "#DD0000", "#FFCE00"],
+        "fondo": "#FFFFFF"
     },
     {
-        "nombre": "Francia",
-        "ids": ["francia", "fr", "fra"],
-        "tipo_bandera": "tipo_2",
-        "colores": ["blue", "white", "red"],
-        "fondo": "white"
+        "nombre": "Armenia",
+        "ids": ["armenia", "am", "arm"],
+        "tipo_bandera": "tipo_1",
+        "colores": ["#D90012", "#0033A0", "#F2A800"],
+        "fondo": "#000000"
+    },
+    {
+        "nombre": "Austria",
+        "ids": ["austria", "at", "aut"],
+        "tipo_bandera": "tipo_1",
+        "colores": ["#ED2939", "#FFFFFF", "#ED2939"],
+        "fondo": "#000000"
+    },
+    {
+        "nombre": "Bolivia",
+        "ids": ["bolivia", "bo", "bol"],
+        "tipo_bandera": "tipo_1",
+        "colores": ["#D52B1E", "#FFD700", "#007A33"],
+        "fondo": "#FFFFFF"
+    },
+    {
+        "nombre": "Bulgaria",
+        "ids": ["bulgaria", "bg", "bgr"],
+        "tipo_bandera": "tipo_1",
+        "colores": ["#FFFFFF", "#00966E", "#D62612"],
+        "fondo": "#000000"
+    },
+    {
+        "nombre": "Ucrania",
+        "ids": ["ucrania", "ua"],
+        "tipo_bandera": "tipo_1",
+        "colores": ["#0057B7", "#FFDD00"],
+        "fondo": "#000000"
+    },
+    {
+        "nombre": "Sierra Leona",
+        "ids": ["sierra leona", "sl", "sle"],
+        "tipo_bandera": "tipo_1",
+        "colores": ["#1EB53A", "#FFFFFF", "#0072C6"],
+        "fondo": "#000000"
+    },
+    {
+        "nombre": "Estonia",
+        "ids": ["estonia", "ee", "est"],
+        "tipo_bandera": "tipo_1",
+        "colores": ["#0072CE", "#000000", "#FFFFFF"],
+        "fondo": "#9b9b9b"
     },
     {
         "nombre": "Italia",
-        "ids": ["italia", "it", "ita"],
+        "ids": ["italia", "it", "ita", "italy"],
         "tipo_bandera": "tipo_2",
-        "colores": ["green", "white", "red"],
-        "fondo": "black"
+        "colores": ["#008C45", "#F4F5F0", "#CD212A"],
+        "fondo": "#000000"
     },
     {
         "nombre": "Benín",
-        "ids": ["benin", "bj"],
+        "ids": ["benin", "bj", "ben"],
         "tipo_bandera": "tipo_3",
-        "colores": ["green", "yellow", "red"],
-        "fondo": "white"
+        "colores": ["#008751", "#FCD116", "#E8112D"],
+        "fondo": "#FFFFFF"
     },
     {
         "nombre": "Japón",
-        "ids": ["japon", "jp", "jpn"],
+        "ids": ["japon", "jp", "jpn", "japan"],
         "tipo_bandera": "tipo_4",
         "colores": None,
-        "fondo": "white"
+        "fondo": "#FFFFFF"
     },
     {
         "nombre": "Finlandia",
-        "ids": ["finlandia", "fi", "fin"],
+        "ids": ["finlandia", "fi", "fin", "finland"],
         "tipo_bandera": "tipo_5",
         "colores": None,
-        "fondo": "white"
+        "fondo": "#FFFFFF"
     }
 ]
